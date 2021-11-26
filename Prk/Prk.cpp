6586ea84b801c4a545b589ed0fa1b10bdbfc02f2
IluaSN <ilua.snegirev.97@mail.ru>
2021-11-26 22:17:59 +1000

    HW5

--- Prk/Prk.cpp
+++ Prk/Prk.cpp
@@ -2,20 +2,49 @@
 
 using namespace std;
 
+bool les5(int iArr5[], const int SIZE5)
+{
+	int SUMM1 = 0,
+		SUMM2 = 0,
+		c = SIZE5-1;
+
+
+	for (int i = 1; i < SIZE5; i++, c--)
+	{
+		SUMM1 += iArr5[i];
+
+		if (i + 1 != c)
+		{
+			SUMM2 += iArr5[c];
+		}
+		else
+			break;
+
+	}
+
+	if (SUMM1==SUMM2)
+	{
+		cout << SUMM1 << " = " << SUMM2 << "-true, checkBalance" << endl;
+		return "true";
+	}
+	else
+	{	
+		cout << SUMM1 << " не = " << SUMM2 << "-false, checkBalance" << endl;
+		return "false";
+	}
+}
+
 int main()
 {
 	setlocale(LC_ALL, "ru");
 
-	cout << "Hello World!" <<endl;
+	const int SIZE5 = 6;
 
+	int iArr5[SIZE5]{ 1, 2, 3, 1, 1,1 };
 
-	/***Написать функцию, которой передается не пустой одномерный целочисленный массив, 
-		она должна вернуть истину если в массиве есть место, в котором сумма левой и 
-		правой части массива равны.Примеры: checkBalance([1, 1, 1, || 2, 1]) → true, checkBalance([2, 1, 1, 2, 1]) → false, 
-		checkBalance([10, || 1, 2, 3, 4]) → true.Абстрактная граница показана символами || , эти символы в массив не входят.*/
+	les5 (iArr5, SIZE5);
 
-
-
+	return 0;
 }
 
 
