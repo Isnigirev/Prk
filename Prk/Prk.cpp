27e9e820fb2b775b0369d0f266cbf7c24ad2d3ac
IluaSN <ilua.snegirev.97@mail.ru>
2021-12-11 01:54:08 +1000

    HW

--- Prk/Prk.cpp
+++ Prk/Prk.cpp
@@ -1,7 +1,7 @@
 ﻿#include <iostream>
 #include <ctime>
 #include <string>
-
+#include <stdlib.h>
 using namespace std;
 //==================================================================================================
 
@@ -11,13 +11,19 @@ enum fg :char
 	X = 'X',
 	O = 'O',
 };
+struct Coord
+{
+	int y;
+	int x;
+};
 #pragma pack (push,1)
 struct Game
 {
-	int iSize = 5, y = 0,x = 0;
+	int iSize = 3;
 	char PL, PC;
 	char** cArr = new char* [iSize];
 	int iTimer;
+	char iWin = 'W', iW = 'W';
 };
 #pragma pack (pop)
 
@@ -34,27 +40,45 @@ void inicArr(int iSize, char** cArr)
 		}
 	}
 }
-void seeArr(int iSize, char** cArr)
+void dellcArr(int iSize, char** cArr)
 {
 	for (int y = 0; y < iSize; y++)
 	{
+		delete[] cArr[y];
+	}
+	delete[] cArr;
+}
+void seeArr(int iSize, char** cArr)
+{
+	cout << "   ";
+	for (int x = 0; x < iSize; x++)
+	{
+		cout << x + 1 << " ";
+	}
+
+	cout << endl;
+
+	for (int y = 0; y < iSize; y++)
+	{
+		cout << " " << y + 1 << "|";
+
 		for (int x = 0; x < iSize; x++)
 		{
-			cout << "|" << cArr[y][x];
+			cout << cArr[y][x] << "|";
 		}
 		cout << endl;
 	}
 }
-int getWin(int iSize, char** cArr, char One)
+int getWin(int iSize, char** cArr, char One, char cW = '_')
 {
 	int getWin = 0;
 
 	for (int y = 0; y < iSize; y++)
 	{
-	//-->
+	//Вправо
 		for (int x = 0; x < iSize; x++)
 		{
-			if (cArr[y][x]==One)
+			if (cArr[y][x]== One)
 			{
 				getWin++;
 			}
@@ -65,11 +89,10 @@ int getWin(int iSize, char** cArr, char One)
 		}
 		if (getWin >= iSize)
 		{
-			return getWin;
-			break;
+			return One;
 		}
 	}
-	//|
+	//Вниз
 	for (int x = 0; x < iSize; x++)
 	{
 		for (int y = 0; y < iSize; y++)
@@ -85,30 +108,32 @@ int getWin(int iSize, char** cArr, char One)
 		}
 		if (getWin >= iSize)
 		{
-			return getWin;
-			break;
+			return One;
 		}
 	}
-	//-|
+	//Вниз и вправо
 	for (int x = 0; x < iSize; x++)
 	{
 		for (int y = 0; y < iSize; y++)
 		{
 			if (cArr[y][x] == One)
 			{
-				for (int i = 0; i < iSize-1; i++)
+				for (int y1 = 0, x1 = 0; y1 < iSize - y; y1++, x1++)
 				{
-					if (cArr[y][x] == One)
+					if (cArr[y1][x1] == One)
 					{
 						getWin++;
-						cArr[y + 1][x + 1];
+					}
+					else
+					{
+						getWin = 0;
+						break;
 					}
 				}
 			}
 			if (getWin >= iSize)
 			{
-			return getWin;
-			break;
+			return One;
 			}
 			else
 			{
@@ -117,26 +142,29 @@ int getWin(int iSize, char** cArr, char One)
 		}
 		
 	}
-	//-|
+	//Вверх и вправо
 	for (int y = iSize - 1; y >= 0; y--)
 	{
 		for (int x = iSize - 1; x >= 0; x--)
 		{
 			if (cArr[y][x] == One)
 			{
-				for (int i = 0; i < iSize; i++)
+				for (int y1 = y, x1 = x; y1 + 1 > 0; x1++,y1--)
 				{
-					if (cArr[y][x] == One)
+					if (cArr[y1][x1] == One)
 					{
 						getWin++;
-						cArr[y - 1][x + 1];
+					}
+					else
+					{
+						getWin = 0;
+						break;
 					}
 				}
 			}
 			if (getWin >= iSize)
 			{
-				return getWin;
-				break;
+				return One;
 			}
 			else
 			{
@@ -144,149 +172,182 @@ int getWin(int iSize, char** cArr, char One)
 			}
 		}
 	}
+	return cW;
 }
-int turnPL(int n, int iSize)
-{
-	cout << "Введите нужную клетку куда Вы хотите поставить фигуру: " << endl
-		 << "(подсказка-->) Вы вводите" << n << "координату" << endl;
-	cin >> n;
-	n--;
+int getNch(int iSize, char** cArr, char One, char cW = '_')
+{	
+	int igetNch = 0;
 
-	if ( n >  iSize)
-	{
-		cout << "Вы введи не верные координаты.... нужно ввести число не превышающее: "
-			 << iSize - 1 << endl;
-		cin >> n;
-		n--;
-		return n;
-	}
-	else
-	{
-		return n;
-	}
-}
-void turnPC(int iSize, char** cArr, char One)
-{
 	for (int y = 0; y < iSize; y++)
 	{
 		for (int x = 0; x < iSize; x++)
 		{
-			if (cArr[y][x]==fg::_)
-			{
-				for (int y1= 0; y1 < iSize; y1++)
-				{
-					for (int x1 = 0; x1 < iSize; x1++)
-					{
-						if (cArr[y1][x1]==One)
-						{//-
-							if (cArr[y1][x1-1]==fg::_ && cArr[y1][x1+1]==One)
-							{
-								cArr[y1][x1 - 1] = One;
-								break;
-							}
-							else
-							{
-								for (int x1 = 0; x1 < iSize-x1; x1++) //?
-								{
-									if (cArr[y1][x1] == fg::_&&cArr[y1][x1-1]==One)
-									{
-										cArr[y1][x1] = One;
-									}
-									else
-									{
-										break;
-									}
-								}
-							}//|
-							if (cArr[y1-1][x1] == fg::_ && cArr[y1+1][x1] == One)
-							{
-								cArr[y1-1][x1] = One;
-								break;
-							}
-							else
-							{
-								for (int y1 = 0; y1 < iSize - y1; y1++) //?
-								{
-									if (cArr[y1][x1] == fg::_ && cArr[y1-1][x1] == One)
-									{
-										cArr[y1][x1] = One;
-									}
-									else
-									{
-										break;
-									}
-								}
-							}
-						}
-					}
-				}
-			}
-			else if (cArr[y][x]==One)
-			{
-				if (cArr[y][x-1] == fg::_&& cArr[y][x+1] == One)
-				{
-					cArr[y][x - 1] = One;
-					break;
-				}
-				else
-				{
-					for (int x1 = 0; x1 < iSize-x; x1++)
-					{
-
-					}
-				}
-			}
-			else
-			{
-				break;
-			}
+			if (cArr[y][x] == fg::_)
+				igetNch++;
 		}
+	}
+	if (igetNch > 0)
+	{
+		return cW;
+	}
+	else
+	{
+		return 'Nch';
 	}
 }
 
-
-
-void main()
+Coord turnPL(Game g)
 {
-	setlocale(LC_ALL, "ru");
-	Game g;
-	inicArr(g.iSize, g.cArr);
-	
-	srand(time(NULL));
-
-	cout << "Выберите фигуру: " << endl
-		 << "x или O (английская раскладка)" << endl;
-
-	//cin >> g.PL;
-	g.PL = fg::O;//delete
-	//(g.PL == 'X' || g.PL == 'x') ? g.PL = fg::X, g.PC = fg::O : g.PL = fg::O, g.PC = fg::X;
-
-	do
-	{	
-		seeArr (g.iSize, g.cArr);
-		
-		if (g.iTimer % 2 == 0)
+	Coord c;
+	cout << "Введите нужную клетку куда Вы хотите поставить фигуру: " << endl
+		 << "(подсказка-->) вводите через пробел (1-у 2-х)" << endl;
+	do {
+	cin >> c.y >> c.x;
+	c.y--, c.x--;
+	if (g.cArr[c.y][c.x] != fg::_)
+		cout << "Вы ввели занятую координату" << endl;
+	} while (c.y >= g.iSize || c.x >= g.iSize || g.cArr[c.y][c.x] != fg::_);
+	return c;
+}
+Coord turnPC(Game g)
+{
+	Coord c;
+	//Для логики
+	for (int y = 0; y < g.iSize; y++)
+	{
+		for (int x = 0; x < g.iSize; x++)
 		{
-			//g.cArr[g.y = turnPL(g.y, g.iSize)][g.x = turnPL(g.x, g.iSize)] ;//?
-			//g.cArr[g.y][g.x] = g.PL;
-			//g.PL = getWin(g.iSize, g.cArr, g.PL);
+			if (g.cArr[y][x] == fg::_)
+			{
+				g.cArr[y][x] = g.PC;
+				if (getWin(g.iSize,g.cArr, g.PC) == g.PC)
+				{
+					g.cArr[y][x] = fg::_;
+					return {y,x};
+				}
+				g.cArr[y][x] = fg::_;
+			}
 		}
-		else
-		{
-			g.PC = fg::O;
-			g.cArr[0][0] = g.PL;
-			turnPC(g.iSize, g.cArr, g.PC);
-		}
-
-		cout << "\033[2J\033[1;1H";
-		g.PL = getWin(g.iSize, g.cArr, g.PL);
-		g.iTimer++;
-
-	} while (g.PL != g.iSize);
+	}
 
 	for (int y = 0; y < g.iSize; y++)
 	{
-		delete [] g.cArr [y];
+		for (int x = 0; x < g.iSize; x++)
+		{
+			if (g.cArr[y][x] == fg::_)
+			{
+				g.cArr[y][x] = g.PL;
+				if (getWin(g.iSize, g.cArr, g.PL) == g.PL)
+				{
+					g.cArr[y][x] = fg::_;
+					return { y,x };
+				}
+				g.cArr[y][x] = fg::_;
+			}
+		}
 	}
-	delete[] g.cArr;
+
+	// Для первого хода
+	srand(time(NULL));
+	do
+	{
+		c.y = rand() % g.iSize;
+		c.x = rand() % g.iSize;
+		g.cArr[c.y][c.x];
+
+	} while (g.cArr[c.y][c.x] != fg::_);
+
+	return c;
+
+	Coord buf[4];
+	int ib{ 0 };
+	if (g.cArr[0][0] == fg::_)
+	{
+		buf[ib] = { 0,0 };
+		ib++;
+	}
+	else if (g.cArr[g.iSize-1][g.iSize - 1] == fg::_)
+	{
+		buf[ib] = { g.iSize - 1, g.iSize - 1};
+		ib++;
+	}
+	else if (g.cArr[0][g.iSize - 1] == fg::_)
+	{
+		buf[ib] = { 0, g.iSize - 1 };
+		ib++;
+	}
+	else if (g.cArr[0][g.iSize - 1] == fg::_)
+	{
+		buf[ib] = { g.iSize - 1, 0 };
+		ib++;
+	}
+}
+
+int main()
+{
+	setlocale(LC_ALL, "ru");
+	Game g;
+	
+	cout << "Введите нужное поле (по умолчанию инициализируется 3х3): " << endl
+		 << "(должно быть от 2 и более)" << endl;
+	do
+	{
+	cin >> g.iSize;
+
+	if (g.iSize < 2 || g.iSize > 6)
+		cout << "Вы ввели недопустимое поле (должно быть от 2 до 6)" << endl;
+	} while (g.iSize < 2 || g.iSize > 6);
+	
+	inicArr(g.iSize, g.cArr);
+
+	cout << "Выберите фигуру: " << endl
+		 << "x или O (английская раскладка)" << endl;
+	cin >> g.PL;
+	if (g.PL == 'X' || g.PL == 'x')
+	{
+		g.PL = fg::X;
+		g.PC = fg::O;
+	}
+	else if (g.PL == 'O' || g.PL == 'o')
+	{
+		g.PL = fg::O;
+		g.PC = fg::X;
+	}
+	else
+	{
+		cout << "Вы выбрали не верный символ.... " << endl;
+		return 0;
+	}
+	
+	do
+	{	
+		seeArr (g.iSize, g.cArr);
+
+		if (g.iTimer % 2 == 0)
+		{
+			//g.cArr[0][4] = g.PL, g.cArr[1][3] = g.PL, g.cArr[2][2] = g.PL, g.cArr[3][1] = g.PL, g.cArr[4][0] = g.PL;
+			Coord c = turnPL(g);
+			g.cArr[c.y][c.x] = g.PL;
+			g.iW = getWin(g.iSize, g.cArr, g.PL, g.iW);
+		}
+		else
+		{
+			Coord c = turnPC(g);
+			g.cArr[c.y][c.x] = g.PC;
+			g.iW = getWin(g.iSize,g.cArr, g.PC,g.iW);
+		}
+
+		cout << "\033[2J\033[1;1H";
+		
+		g.iTimer++;
+
+		g.iW = getNch(g.iSize, g.cArr, g.PC, g.iW);
+
+	} while (g.iWin == g.iW);
+	
+	cout << "Выйграла: " << g.iW << endl;
+
+	dellcArr(g.iSize, g.cArr);
+
+	return 0;
 }
