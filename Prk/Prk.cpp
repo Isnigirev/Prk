ea9f86e0d711bec79b08ac41d5a2017e8ce47cce
IluaSN <ilua.snegirev.97@mail.ru>
2022-02-23 16:08:57 +1000

    ///

--- Prk/Prk.cpp
+++ Prk/Prk.cpp
@@ -40,7 +40,6 @@ void depthTravers(T st)
 }
 //1.//////////////////////////////////
 
-
 void resetStack()
 {
     for (int i = 0; i < SIZE2; i++)
@@ -48,6 +47,152 @@ void resetStack()
         Stack[i] = 0;
     }
 }
+
+//2.//////////////////////////////////
+typedef struct {
+    struct TwoLinkNode* head;
+    struct TwoLinkNode* tail;
+    int size;
+} TwoLinkList;
+
+void initTwoLinkList(TwoLinkList* lst) {
+    lst->head = NULL;
+    lst->tail = NULL;
+    lst->size = 0;
+}
+
+typedef struct TwoLinkNode {
+    int dat;
+    struct TwoLinkNode* next;
+    struct TwoLinkNode* prev;
+} TwoLinkNode;
+
+boolean TwoLinkEnqueue(TwoLinkList* queue, T value) {
+    TwoLinkNode* tmp = (TwoLinkNode*)malloc(sizeof(TwoLinkNode));
+    if (tmp == NULL) {
+        printf("Out of memory! \n");
+        return false;
+    }
+    tmp->dat = value;
+    tmp->next = NULL;
+    tmp->prev = queue->tail;
+
+    if (queue->tail != NULL) {
+        queue->tail->next = tmp;
+    }
+    if (queue->head == NULL) {
+        queue->head = tmp;
+    }
+
+    queue->tail = tmp;
+    queue->size++;
+    return true;
+}
+
+T TwoLinkDequeue(TwoLinkList* queue) {
+    if (queue->size == 0) {
+        printf("Queue is empty \n");
+        return -1;
+    }
+    TwoLinkNode* tmp = queue->head;
+    T data = queue->head->dat;
+
+    queue->head = queue->head->next;
+
+    if (queue->head != NULL) {
+        queue->head->prev = NULL;
+    }
+    if (tmp == queue->tail) {
+        queue->tail = NULL;
+    }
+
+    free(tmp);
+    queue->size--;
+    return data;
+}
+
+int SimpleGraph[SIZE2][SIZE2] = {
+          {0,1,0,0,0,0},
+          {0,0,1,0,0,0},
+          {0,0,0,1,0,0},
+          {0,0,0,0,1,0},
+          {0,0,0,0,0,1},
+          {0,0,0,0,0,0}};
+
+int get2dInt(int row, int col)
+{
+    matrix[SIZE2][SIZE2];
+
+    return matrix[row][col];
+}
+
+//////////////////////////////////////
+
+int Counter = 0;
+
+void DepthTraversSimpleGraph(T st, int indx)
+{
+    int r;
+
+    cout << st;
+
+    for (int r = 0; r < SIZE2; ++r)
+    {
+        if (st == indx && matrix[st][r] == 1)
+        {
+            Stack[r] = 1;
+            Counter++;
+        }
+
+        if (matrix[st][r] == 1 && !Stack[r])
+        {
+            cout << "->";
+            DepthTraversSimpleGraph(r, indx);
+        }
+    }
+}
+
+void WidthTraversSimpleGraph(int start, int indxx,int &Counter)
+{
+    TwoLinkList* queue = (TwoLinkList*)malloc(sizeof(TwoLinkList));
+    initTwoLinkList(queue);
+    TwoLinkEnqueue(queue, start);
+
+    while (queue->size > 0)
+    {
+        int indx = TwoLinkDequeue(queue);
+        if (Stack[indx] == 1) continue;
+        Stack[indx] = 1;
+
+        for (int i = 0; i < SIZE2; ++i)
+        {
+            if (start == indxx && matrix[start][i] == 1 && !Stack[i])
+            {
+                Stack[i] = i;
+                Counter++;
+            }
+
+            if (get2dInt(indx, i) == 1)
+            {
+                TwoLinkEnqueue(queue, i);
+            }
+        }
+    }
+}
+
+void StackPop()
+{
+    cout << "\n";
+    for (int i = 0; i < SIZE2; i++)
+    {
+        cout << Stack[i];
+    }
+}
+
+//////////////////////////////////////
+
+//обход графа по матрице смежности(с подсчётом всех вершин графа) 
+//В конце обхода вывести два получившихся списка всех узлов в порядке уменьшения количества ссылок на них.
 
 int main()
 {
@@ -64,8 +209,23 @@ int main()
     depthTravers(1);
     resetStack();
 //1.//////////////////////////////////
-
-
+    cout << "\n";
+//2.//////////////////////////////////
+    cout << "Обход в глубину" << endl;
+    for (int i = 0; i < SIZE2; i++)
+    {
+        cout << endl; resetStack();
+        DepthTraversSimpleGraph(i, i);
+        cout << "\nВсего смежных величи с " << i << "->" << Counter;
+    }
+    cout << "\nОбход в ширину" << endl;
+    for (int i = 0; i < SIZE2; i++)
+    {
+        resetStack();
+        WidthTraversSimpleGraph(i, i, Counter = 0);
+        StackPop();
+        cout << "\nВсего смежных величи с " << i << "->" << Counter;
+    }
 
     return 0;
 }
