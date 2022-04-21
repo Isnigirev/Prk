4a78cb16b54f725f1c6b57eb4016b6be24aa600c
IluaSN <ilua.snegirev.97@mail.ru>
2022-04-22 00:22:50 +1000

    8

--- Prk/Prk.cpp
+++ Prk/Prk.cpp
@@ -1,193 +1,219 @@
 ﻿#include <iostream>
-#include <vector>
+#include <string>
 
-/*
-4.Создать класс Card, описывающий карту в игре БлэкДжек. 
-У этого класса должно быть три поля: масть, 
-значение карты и положение карты (вверх лицом или рубашкой). 
-Сделать поля масть и значение карты типом перечисления (enum). 
-Положение карты - тип bool. Также в этом классе должно быть два метода:
-метод Flip(), который переворачивает карту, 
-т.е. если она была рубашкой вверх, то он ее поворачивает лицом вверх, 
-и наоборот.
-метод GetValue(), который возвращает значение карты, пока можно считать, 
-что туз = 1.
-*/
-
-class Card
+//1
+class DivisionByZero
 {
-    enum Suit
-    {
-        Worms,
-        Diamonds,
-        Clubs,
-        Peaks
-    };
-
-    enum MapValue
-    {
-        Two,
-        Three,
-        Four,
-        Five,
-        Six,
-        Seven,
-        Eight,
-        Hine,
-        Ten,
-        J,
-        O,
-        K,
-        A,
-    };
-
-    bool stMapMosition; //(1 - face up 0 - shirt up)
+	std::string Message;
 
 public:
-    bool Flip(bool stMapMosition)
-    {
-        if (stMapMosition == 1)
-        {
-            std::cout << "Карта перевернута рубашкой вверх\n";
-            return stMapMosition = 0;
-        }
-        else
-        {
-            std::cout << "Карта перевернута лицом вверх\n";
-            return stMapMosition = 1;
-        }
-    }
+	DivisionByZero(std::string message = "") : Message(message) {}
 
-    const int GetValue(int a)
-    {
-        switch (a)
-        {
-        case Two:
-            return 2;
-        case Three:
-            return 3;
-        case Four:
-            return 4;
-        case Five:
-            return 5;
-        case Six:
-            return 6;
-        case Seven:
-            return 7;
-        case Eight:
-            return 8;
-        case Hine:
-            return 9;
-        case Ten:
-            return 10;
-        case J:
-            return 10;
-        case O:
-            return 10;
-        case K:
-            return 10;
-        case A:
-            return 1;
-        default:
-            break;
-        }
-    }
+	const std::string& GetMessage() 
+	{ 
+		return Message;
+	}
 };
 
-class Hand
+template <typename T>
+T Div(T a, T b)
 {
-    std::vector <Card> vCard;
+	if (b == 0)
+	{
+		throw DivisionByZero("Деление на ноль!");
+	}	
 
+	return a / b;
+}
+
+//2
+template <typename T1>
+class Ex
+{
 public:
-    void Add(Card)
-    {
-        std::vector <Card> vCard;
-    }
+	Ex(T1 x) : X(x) {};
 
-    void Clear()
-    {
-
-    }
-
-    void GetValue()
-    {
-
-    }
+	T1 X;
 };
 
-class GenericPlayer : public Hand
+template <typename T3>
+class Bar
 {
-    std::string stName;
+	T3 Y;
 
 public:
-    GenericPlayer(std::string stName)
-    {
-        this->stName = stName;
-    }
+	Bar(T3 y = 0) : Y (y = 0) {}
 
-    virtual bool IsHitting() 
-    {
-        char a;
-        std::cout << "Взять карту? (y/n) \n";
-        std::cin >> a;
-
-        return (a == 'y' || a == 'Y') ? 1 : 0;
-    }
-
-    bool IsBoosted()
-    {
-
-    }
-
-    void Bust()
-    {
-        std::cout << stName << "\tперебор!\n";
-    }
-
-    void GetName()
-    {
-        std::cout << stName << std::endl;
-    }
+	void Set(T3 a)
+	{
+		((Y + a) > 100) ? throw Ex <T3>(a * Y) : Y = a;
+	}
 };
 
-class Player : public GenericPlayer
+////3
+class OffTheField
 {
+	int CurPosX;
+	int CurPosY;
+	int NewPosX;
+	int NewPosY;
+	std::string Message;
+
 public:
-    virtual bool isHitting() const {}
+	OffTheField(int CX, int CY, int NX, int NY) :
+		CurPosX(CX), CurPosY(CY), NewPosX(NX), NewPosY(NY), Message("")
+	{
+		if (NewPosX > 10 || NewPosX < 1)
+		{
+			Message += "Ошибка координаты X! Робот выходит за границу поля!\n";
+			Message += '[' + std::to_string(CurPosX) + ';' + std::to_string(CurPosY) + "] => [" +
+				std::to_string(NewPosX) + ';' + std::to_string(NewPosY) + "]\n";
+		}
+		if (NewPosY > 10 || NewPosY < 1)
+		{
+			Message += "Ошибка координаты Y! Робот выходит за границу поля!\n";
+			Message += '[' + std::to_string(CurPosX) + ';' + std::to_string(CurPosY) + "] => [" +
+				std::to_string(NewPosX) + ';' + std::to_string(NewPosY) + "]\n";
+		}
+	}
 
-    void Win() const 
-    {
-        GenericPlayer::GetName;
-        std::cout << " Выйграл!" << std::endl;
-    }
-
-    void Lose() const
-    {
-        GenericPlayer::GetName;
-        std::cout << " Проиграл!" << std::endl;
-    }
-
-    void Push() const
-    {
-        GenericPlayer::GetName;
-        std::cout << " Сыграл в ничью!" << std::endl;
-    }
+	const std::string& GetInfo() const
+	{
+		return Message;
+	}
 };
 
-class House : public GenericPlayer
+class IllegalCommand
 {
-public:
-    virtual bool isHitting() const {} // working...
+	int CurPosX;
+	int CurPosY;
+	int NewPosX;
+	int NewPosY;
+	std::string Message;
 
-    void FlipFirstCard()
-    {
-        Card::Flip;
-    }
+public:
+	IllegalCommand(int CX, int CY, int NX, int NY) :
+		CurPosX(CX), CurPosY(CY), NewPosX(NX), NewPosY(NY), Message("")
+	{
+		if (abs(CurPosX - NewPosX) > 1)
+		{
+			Message += "Ошибка координаты X! Шаг слишком велик!\n";
+			Message += '[' + std::to_string(CurPosX) + ';' + std::to_string(CurPosY) + "] => [" +
+				std::to_string(NewPosX) + ';' + std::to_string(NewPosY) + "]\n";
+		}
+
+		if (abs(CurPosY - NewPosY) > 1)
+		{
+			Message += "Ошибка координаты Y! Шаг слишком велик!\n";
+			Message += '[' + std::to_string(CurPosX) + ';' + std::to_string(CurPosY) + "] => [" +
+				std::to_string(NewPosX) + ';' + std::to_string(NewPosY) + "]\n";
+		}
+
+		if (CurPosX == NewPosX && CurPosY == NewPosY)
+		{
+			Message += "Ошибка координаты X и Y! Нельзя шагнуть на текущее место!\n";
+			Message += '[' + std::to_string(CurPosX) + ';' + std::to_string(CurPosY) + "] => [" +
+				std::to_string(NewPosX) + ';' + std::to_string(NewPosY) + "]\n";
+		}
+	}
+
+	const std::string& GetInfo() const
+	{
+		return Message;
+	}
+};
+
+class Robot
+{
+	int PosX;
+	int PosY;
+
+public:
+	Robot(int x = 5, int y = 5) : PosX(x), PosY(y)
+	{
+		std::cout << "Начальная позиция робота: [" << PosX << ";" << PosY << "]\n";
+	}
+
+	void Move(int x, int y)
+	{
+		if (x > 10 || x < 1 || y > 10 || y < 1)
+		{
+			throw OffTheField(PosX, PosY, x, y);
+		}
+		if (abs(PosX - x) > 1 || abs(PosY - y) > 1 || (PosX == x && PosY == y))
+		{
+			throw IllegalCommand(PosX, PosY, x, y);
+		}
+				
+		std::cout << "Робот шагнул [" << PosX << ";" << PosY << "] => [" << x << ";" << y << "]\n";
+
+		PosX = x;
+		PosY = y;
+	}
 };
 
 int main()
 {
     setlocale(LC_ALL, "Ru");
+
+//1
+	try
+	{
+		double result = Div(10, 0);
+		std::cout << result << std::endl;
+	}
+	catch (DivisionByZero& e)
+	{
+		std::cerr << e.GetMessage() << std::endl;
+	}
+
+//2
+	Bar <int> b;
+	int n;
+
+	try
+	{
+		while (true)
+		{
+			std::cin >> n;
+			if (n == 0)
+			{
+				break;
+			}
+
+			b.Set(n);
+		}
+	}
+	catch (Ex <int> e)
+	{
+		std::cout << e.X << std::endl;
+	}
+//3
+	int X = 0, Y = 0;
+	Robot r;
+
+	while (true)
+	{
+		std::cin >> X >> Y;
+
+		if (X == 0 && Y == 0)
+		{
+			break;
+		}
+	
+		try
+		{
+			r.Move(X, Y);
+		}
+		catch (const IllegalCommand& e)
+		{
+			std::cerr << e.GetInfo() << std::endl;
+		}
+		catch (const OffTheField& e)
+		{
+			std::cerr << e.GetInfo() << std::endl;
+		}
+	}
 
     return 0;
 }
